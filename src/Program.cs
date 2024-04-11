3fe326c56686f3e1c2dd442fdddb4516e2b93e83
Werner Mairl <werner.mairl@gmail.com>
2024-04-11 07:10:18 +0200

    multi-process draft

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Data.Common;
 using System.Diagnostics;
 using System.Globalization;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using PerfDemo.OsmFormat;
@@ -13,10 +16,11 @@ namespace PerfDemo
     /// </summary>
     public static class Program
     {
-        public static async Task MeasureAsync(MeasurementInputs inputs, CancellationToken cancellationToken)
+        public static async Task MeasureAsync(MeasurementInputs inputs, int processid, CancellationToken cancellationToken)
         {
             Debug.Assert(inputs.Concurrency > 0);
             Task[] tasks = new Task[inputs.Concurrency];
+            Debug.Assert(inputs.Concurrency == 1);
             int serializationsPerThread = inputs.DeSerializationRequests / inputs.Concurrency;
             for (int i = 0; i < inputs.Concurrency; i++)
             {
@@ -37,7 +41,9 @@ namespace PerfDemo
                     }
                     watch.Stop();
                     var rate = serializationsPerThread / watch.Elapsed.TotalSeconds;
-                    Console.WriteLine($"ThreadId {Environment.CurrentManagedThreadId} takes {watch.ElapsedMilliseconds} ms for {serializationsPerThread} deserialization calls ({Convert.ToInt32(rate)} per second)");
+                    //Console.WriteLine($"{processid.ToString().PadLeft(5)}: ThreadId {Environment.CurrentManagedThreadId} takes {watch.ElapsedMilliseconds} ms for {serializationsPerThread} deserialization calls ({Convert.ToInt32(rate)} per second)");
+                    Console.WriteLine($"{processid.ToString().PadLeft(5)}: {inputs.DeSerializationRequests} takes {watch.ElapsedMilliseconds} ms ({Convert.ToInt32(rate)} per second)");
+
                 },
                cancellationToken,
                TaskCreationOptions.None,  //investigate impact of LongRunning 
@@ -63,35 +69,86 @@ namespace PerfDemo
         /// <summary>
         /// execute measurements for all this number of tasks/threads
         /// </summary>
-        private static readonly int[] Concurrencies = new int[] { 1, 2, 3, 4, 8 };
+        private static readonly int[] Concurrencies = new int[] { 1 };// new int[] { 1, 2, 3, 4, 8 };
+
+        public static int SubProcesses { get; set; } = 1;
 
         /// <summary>
         /// how many deserializations should be done overall (splitted over n tasks/threads
         /// </summary>
-        private const int DeserializationRequests = (ExpectedNodeCreations / SerializedSample) * 4;
-
+        public static int DeserializationRequests => ((ExpectedNodeCreations / SerializedSample) * 8 * 10) / SubProcesses;
         public static async Task<int> Main(string[] args)
         {
-            Console.WriteLine("PerfDemo v1.2.0");
-            Console.WriteLine();
-            Console.WriteLine($"  Processors (available): {Environment.ProcessorCount}");
-            Console.WriteLine($"  Osm-Nodes to deserialize: {ExpectedNodeCreations.ToString("#,###,##0", CultureInfo.InvariantCulture)}");
-            Console.WriteLine();
-            Console.WriteLine($"  Press Ctrl+C or Ctrl+Break for cancel!");
-            Console.WriteLine();
             Debug.Assert(args != null);
+            var currentProcess = Process.GetCurrentProcess();
+            bool noLogo = false;
+            bool quiet = false;
+            bool doWork = args.Where(a => string.Equals(a, "--NoProc", StringComparison.InvariantCultureIgnoreCase)).Any();
+            Program.SubProcesses = 10;
+            if (doWork)
+            {
+                noLogo = true;
+                quiet = true;
+            }
+            Debug.Assert(SubProcesses > 0);
+            if (!noLogo)
+            {
+                Console.ResetColor();
+                Console.WriteLine("PerfDemo v2.0.0");
+                Console.WriteLine();
+            }
+
+            if (!quiet)
+            {
+                Console.ResetColor();
+                Console.WriteLine($"  Processors (available): {Environment.ProcessorCount}");
+                Console.WriteLine($"  Processes (started): {SubProcesses}");
+
+                Console.WriteLine($"  Process: {currentProcess.Id} (Priority={currentProcess.BasePriority})");
+
+                Console.WriteLine($"  Osm-Nodes to deserialize: {ExpectedNodeCreations.ToString("#,###,##0", CultureInfo.InvariantCulture)}");
+                Console.WriteLine();
+                Console.WriteLine($"  Press Ctrl+C or Ctrl+Break for cancel!");
+                Console.WriteLine();
+            }
+
             var cts = new CancellationTokenSource();
             Console.CancelKeyPress += (s, e) =>
             {
-                Console.ResetColor();
+                //Console.ResetColor();
                 Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Canceling...");
-                Console.ResetColor();
+                Console.WriteLine($"Canceling ({currentProcess.Id})...");
+                //Console.ResetColor();
                 cts.Cancel();
                 e.Cancel = true;
             };
             try
             {
+
+                if (!doWork)
+                {
+                    //execute via multiple processes
+                    Task<Results>[] tasks = new Task<Results>[SubProcesses];
+                    string exeName = Assembly.GetEntryAssembly()!.Location;
+                    if (exeName.EndsWith(".dll"))
+                    {
+                        exeName = exeName.Replace(".dll", ".exe");
+                    }
+                    Debug.Assert(System.IO.File.Exists(exeName));
+                    var pi = new ProcessStartInfo(exeName, "--NoProc");
+                    var sw = Stopwatch.StartNew();
+                    for (int i = 0; i < SubProcesses; i++)
+                    {
+                        tasks[i] = ProcessAyncHelper.RunProcessAsync(pi, cts.Token);
+                    }
+                    await Task.WhenAll(tasks);
+                    sw.Stop();
+                    Console.ResetColor();
+                    Console.WriteLine();
+                    Console.WriteLine($"MultiProcess-Duration: {sw.Elapsed}");
+                    Console.ResetColor();
+                    return 0;
+                }
 
                 TypeModel protoBufModel = ProtoBufTypeInfo.CreateOsmFormatModel(compile: true);
 
@@ -109,17 +166,19 @@ namespace PerfDemo
                 {
                     inputs.Concurrency = concurrency;
                     long lockContentionBefore = Monitor.LockContentionCount;
-                    Console.ResetColor();
-                    Console.WriteLine($"ConcurrentTasks={inputs.Concurrency}, Nodes={inputs.ExpectedSamples}");
-                    Console.ForegroundColor = ConsoleColor.Green;
-
+                    if (!quiet)
+                    {
+                        Console.ResetColor();
+                        Console.WriteLine($"{currentProcess.Id.ToString().PadLeft(5)}: ConcurrentTasks={inputs.Concurrency}, BlockSize={inputs.ExpectedSamples} Nodes");
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
                     var watch = Stopwatch.StartNew();
-                    await MeasureAsync(inputs, cts.Token);
+                    await MeasureAsync(inputs, currentProcess.Id, cts.Token);
                     watch.Stop();
 
                     long lockContentionAfter = Monitor.LockContentionCount;
                     Console.ResetColor();
-                    Console.WriteLine($"Duration={Convert.ToInt64(watch.Elapsed.TotalMilliseconds)} ms, Lock Contention: {lockContentionAfter - lockContentionBefore}");
+                    Console.WriteLine($"{currentProcess.Id.ToString().PadLeft(5)}: Duration={Convert.ToInt64(watch.Elapsed.TotalMilliseconds)} ms, Lock Contention: {lockContentionAfter - lockContentionBefore}");
                     Console.WriteLine();
                 }
                 return 0;
@@ -135,6 +194,7 @@ namespace PerfDemo
                     Console.ResetColor();
                     Console.WriteLine();
                 }
+                Console.ResetColor();
                 return 1;
             }
         }
